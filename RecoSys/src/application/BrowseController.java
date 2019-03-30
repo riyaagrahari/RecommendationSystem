4e079a890622a265f42c992cf80b9eea35988de8
Riya Agrahari <Riya Agrahari@10.12.42.212>
2019-03-30 21:22:21 +0530

    added toggle group

--- RecoSys/src/application/BrowseController.java
+++ RecoSys/src/application/BrowseController.java
@@ -9,16 +9,22 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
-import javafx.stage.Window;
+//import javafx.stage.Window;
 
 public class BrowseController {
 	
 	private Button doneButton;
-
+	private RadioButton rb1 = new RadioButton("Shoes");
+	private RadioButton rb2 = new RadioButton("Shoes");
+	private RadioButton rb3 = new RadioButton("Shoes");
+	final ToggleGroup group = new ToggleGroup();
+	
 	@FXML
     protected void BrowseButtonAction(ActionEvent event) {
-        
+		
         Parent root1 = null;
         FXMLLoader loader2 = null;
 		try {
@@ -33,5 +39,11 @@ public class BrowseController {
 	        window.setScene(scene1);
 	        window.show();
 
+}
+	@FXML
+	protected void RadioClickAction(ActionEvent event) {
+		rb1.setToggleGroup(group);
+		rb2.setToggleGroup(group);
+		rb3.setToggleGroup(group);
 }
 }
--- RecoSys/src/application/WelcomeController.java
+++ RecoSys/src/application/WelcomeController.java
@@ -9,19 +9,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+//import javafx.scene.control.PasswordField;
+//import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.Window;
+//import javafx.stage.Window;
 
 public class WelcomeController {
     @FXML
     private Label name;
     public Label display;
-    private Button logout;
+//    private Button logout;
     //Receive message from scene 1
     public void sendMessage(String message) {
         //Display the message
